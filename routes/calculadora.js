3b536854f8b4870eba6e05db854b531edab5cee1
Vinicius Carvalho Silva <68484993+carvalhovini@users.noreply.github.com>
2023-10-31 15:48:13 -0300

    Delete routes directory

--- routes/calculadora.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const calculadoraController = require('../controllers/calculadoraController');
-
-router.get('/', calculadoraController.mostrarFormulario);
-router.post('/calcular', calculadoraController.calcular);
-
-module.exports = router;
