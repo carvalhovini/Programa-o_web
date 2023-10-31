0d844afc23115d86e12641b94f1edd7f8133f445
Vinicius Carvalho Silva <68484993+carvalhovini@users.noreply.github.com>
2023-10-31 15:47:11 -0300

    Add files via upload

---
+++ routes/calculadora.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const calculadoraController = require('../controllers/calculadoraController');
+
+router.get('/', calculadoraController.mostrarFormulario);
+router.post('/calcular', calculadoraController.calcular);
+
+module.exports = router;
